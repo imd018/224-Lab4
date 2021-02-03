2452d508f1c9df14de044365e2865b16b358777c
imd108 <ics20-077@camosun.bc.ca>
2021-02-03 13:09:14 -0800

    Initial Commit

---
+++ Lab4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab4
+//
+//  Created by ICS 224 on 2021-02-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab4/Lab4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4App.swift
+//  Lab4
+//
+//  Created by ICS 224 on 2021-02-03.
+//
+
+import SwiftUI
+
+@main
+struct Lab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
